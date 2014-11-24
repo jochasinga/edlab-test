3130523b3e63af1e0ddb0120bf59302b1f129bc8
Joe Chasinga <jo.chasinga@gmail.com>
2014-11-24 09:36:46 -0500

    Add server data population

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,15 +1,18 @@
 if (Mocks.find().count() === 0) {
     Mocks.insert({
+        count: 1,
         title: 'Mock #1',
         url: 'http://mock-1.com'
     });
 
     Mocks.insert({
+        count: 2,
         title: 'Mock #2',
         url: 'http://mock-2.com'
     });
 
     Mocks.insert({
+        count: 3,
         title: 'Mock #3',
         url: 'http://mock-3.com'
     });
