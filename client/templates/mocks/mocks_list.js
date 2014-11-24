7eed5e51e7988cd6c921a869618951efc907dd0b
Joe Chasinga <jo.chasinga@gmail.com>
2014-11-24 11:01:20 -0500

    More work on the mock objects

--- client/templates/mocks/mocks_list.js
+++ client/templates/mocks/mocks_list.js
@@ -4,23 +4,25 @@ Template.mocksList.helpers({
     }
 });
 
-Template.mockItem.helpers = function() {
-    if (count === 1) {
-        $('#thumbnail').attr({
-            'data--0-center-top': 'opacity: 0; transform: translate(0px, 50px);',
-            'data--100-center-top': 'opacity: 0; transform: translate(0px, 50px);'
-        });
+Template.mockItem.helpers({
+    attributes: function() {
+        if (count === 1) {
+            return {
+                "data--0-center-top": "opacity: 0; transform: translate(0px, 50px);",
+                "data--100-center-top": "opacity: 0; transform: translate(0px, 50px);"
+            }
+        }
+        else if (count === 2) {
+            return {
+                "data--50-center-top": "opacity: 0; transform: translate(0px, 50px);",
+                "data--150-center-top": "opacity: 0; transform: translate(0px, 50px);"
+            }
+        }
+        else if (count === 3) {
+            return {
+                "data--100-center-top": "opacity: 0; transform: translate(0px, 50px);",
+                "data--200-center-top": "opacity: 0; transform: translate(0px, 50px);"
+            }
+        }
     }
-    else if (count === 2) {
-        $('#thumbnail').attr({
-            'data--50-center-top': "opacity: 0; transform: translate(0px, 50px);",
-            'data--150-center-top': "opacity: 0; transform: translate(0px, 50px);"
-        });
-    }
-    else if (count === 3) {
-        $('#thumbnail').attr({
-            'data--100-center-top': "opacity: 0; transform: translate(0px, 50px);",
-            'data--200-center-top': "opacity: 0; transform: translate(0px, 50px);"
-        });
-    }
-};
+});
