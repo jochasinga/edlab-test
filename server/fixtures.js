642f96205b674977ae910cbaab36d1aaddfdd959
Joe Chasinga <jo.chasinga@gmail.com>
2014-11-24 01:17:58 -0500

    Add data to the mocks

---
+++ server/fixtures.js
@@ -0,0 +1,19 @@
+if (Mocks.find().count() === 0) {
+    Mocks.insert({
+        title: 'Mock #1',
+        url: 'http://mock-1.com'
+    });
+
+    Mocks.insert({
+        title: 'Mock #2',
+        url: 'http://mock-2.com'
+    });
+
+    Mocks.insert({
+        title: 'Mock #3',
+        url: 'http://mock-3.com'
+    });
+}
+
+
+
